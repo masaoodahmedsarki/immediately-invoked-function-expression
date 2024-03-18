2ad3ad3ab207ca64644c8a48384a8e8db182f90f
Masood Ahmed56 <115347725+masaoodahmedsarki@users.noreply.github.com>
2024-03-19 00:12:12 +0500

    immediatelly invoked function expression

---
+++ demo24.js
@@ -0,0 +1,11 @@
+// *************immediately invoked function expression******************
+
+
+(function chai(){
+    console.log(`DB CONNECTED`)
+})();
+
+((name)=>{
+    console.log(`DVB CONNECTED TWO ${name}`);
+})("Noreen")
+
